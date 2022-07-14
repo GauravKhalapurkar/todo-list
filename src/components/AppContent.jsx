a7cd95341e0a3ce5939d63b6dc192c713d4e4dc9
Gaurav K <gaurav.khalapurkar@gmail.com>
2022-07-14 23:02:22 +0530

    getting saved title done

--- src/components/AppContent.jsx
+++ src/components/AppContent.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const AppContent = () => {
-  return <div className="max-w-[800px] mx-auto">AppContent</div>;
+  const todoList = useSelector((state) => state.todo.todoList);
+  const sortedTodoList = [...todoList];
+  sortedTodoList.sort((a, b) => new Date(b.time) - new Date(a.time));
+
+  return (
+    <div className="max-w-[800px] mx-auto">
+      {sortedTodoList.length > 0
+        ? sortedTodoList.map((todo) => todo.title)
+        : "Hurry! No Todos."}
+    </div>
+  );
 };
 
 export default AppContent;
